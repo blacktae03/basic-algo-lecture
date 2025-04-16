f47fddb76b6157b16daa28b0b198da206bcdab6a
blacktae03 <blacktae03@naver.com>
2025-04-16 23:24:14 +0900

    Update test.cpp

--- 0x01/test.cpp
+++ 0x01/test.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int func1(int N){
-  return -1;
+  int sum = 0;
+  for (int i = 1; i <= N; i++)
+    if (i % 3 == 0 || i % 5 == 0) sum += i;
+
+  return sum;
 }
 
 int func2(int arr[], int N){
